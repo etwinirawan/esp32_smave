5c149962154f307aafb30c01bcddba7e63d6949c
etwinirawan <etwinhendri@gmail.com>
2023-12-20 10:35:52 +0700

    reqtoken

--- SMAVE.ino
+++ SMAVE.ino
@@ -14,12 +14,16 @@
 
 const char *ssid = "asli";
 const char *password = "123412345";
-const char *serverAddress = "https://0764m0hj-8080.asse.devtunnels.ms";
+const char *urlSendData = "https://0764m0hj-8080.asse.devtunnels.ms/api/sv1/input-data";
 const char *urlBacaRef = "https://0764m0hj-8080.asse.devtunnels.ms/api/sv1/baca-ref";
+const char *urlReqToken = "https://0764m0hj-8080.asse.devtunnels.ms/api/sv1/req-token";
 const int serverPort = 8080;
 
 String valve = "";
+String reqToken = "";
 boolean dataSent = true;
+
+int countSendToServer = 0;
 
 /// WATERFLOW
 long currentMillis = 0;
@@ -34,19 +38,18 @@ unsigned int flowMilliLitres;
 // unsigned long totalMilliLitres;
 float totalMilliLitres;
 
+int volume;
+
 // Simpan nilai-nilai sebelumnya
 float previousFlowRate = 0.0;
 float previousTotalMilliLitres = 0.0;
 
 // Inisialisasi waktu terakhir penyimpanan
-unsigned long lastSaveTime = 0;  
-
+unsigned long lastSaveTime = 0;
 
 WiFiUDP ntpUDP;
 //////////////waktu(wib)
 NTPClient timeClient(ntpUDP, "pool.ntp.org", 25200);
-
-
 
 void IRAM_ATTR pulseCounter() {
   pulseCount++;
@@ -87,6 +90,9 @@ void writeFile(fs::FS &fs, const char *path, const char *message) {
 void setup() {
   Serial.begin(115200);
 
+  pinMode(SENSOR, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
+
   // Menghubungkan ke WiFi
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) {
@@ -104,15 +110,29 @@ void setup() {
   // Inisialisasi NTPClient
   timeClient.begin();
 
-  pinMode(SENSOR, INPUT_PULLUP);
+  // baca ref
+  bacaRef();
 
-  pulseCount = 0;
-  flowRate = 0.0;
-  flowMilliLitres = 0;
-  totalMilliLitres = 0;
-  previousMillis = 0;
+  // volume = fs
+  // volume = readFile(LittleFS, "/volume.txt");
+  // Buka file "/volume.txt" untuk dibaca
+  File file = LittleFS.open("/volume.txt", "r");
 
-  attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
+  // Periksa apakah file berhasil dibuka
+  if (file) {
+    // Baca nilai dari file dan konversi ke integer
+    volume = file.readString().toInt();
+
+    // Tampilkan nilai di Serial Monitor (opsional)
+    Serial.println(volume);
+
+    // Tutup file setelah selesai membaca
+    file.close();
+  } else {
+    // Gagal membuka file, lakukan sesuatu di sini
+    Serial.println("Gagal membuka file");
+  }
+
 
   delay(1000);
 }
@@ -121,10 +141,15 @@ void loop() {
   // Memperbarui waktu dari server NTP
   timeClient.update();
 
+  if (valve == "on") {
+    waterFlow();
+  }
+
   // Mengirim data jika waktu saat ini adalah kelipatan 5 menit dan data belum dikirim
-  if (timeClient.getMinutes() % 5 == 0) {
+  if (timeClient.getMinutes() % 2 == 0) {
     if (dataSent == true) {
-      sendToNodeJS();
+      reqTokenFromServer();
+      sendToServer();
       dataSent = false;  // Set flag bahwa data telah dikirim
       Serial.println("Send data to server");
     }
@@ -146,47 +171,18 @@ void loop() {
       Serial.println("");
     }
     if (data.equals("kirim")) {
-      sendToNodeJS();
+      sendToServer();
     }
     if (data.equals("bacaref")) {
       bacaRef();
     }
+    if (data.equals("req")) {
+      reqTokenFromServer();
+    }
   }
-  if (valve == "on") {
-    waterFlow();
-  }
-  if (Serial.available() > 0) {
-    String data = Serial.readString();
-  }
-  // Serial.println(timeClient.getMinutes() % 5 == 0);
-  delay(1000);  // Tunggu 1 detik sebelum membaca waktu lagi
 }
 
-void sendToNodeJS() {
-  // Membuat objek HTTPClient
-  HTTPClient http;
 
-  // Menyiapkan URL server Node.js
-  String url = "http://" + String(serverAddress) + ":" + String(serverPort) + "/data";
-
-  // Menyiapkan data yang akan dikirim
-  String data = "debit=8&volume=12";  // Ganti dengan data sesuai kebutuhan
-
-  // Membuat permintaan HTTP POST
-  http.begin(serverAddress);
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-
-  // Mengirim data
-  int httpResponseCode = http.GET();
-
-  // Menampilkan hasil respons server
-  Serial.print("HTTP Response Code: ");
-  Serial.println(httpResponseCode);
-  String response = http.getString();
-  Serial.println(response);
-  // Menutup koneksi
-  http.end();
-}
 
 void bacaRef() {
   HTTPClient http;
@@ -233,26 +229,18 @@ void bacaRef() {
 void waterFlow() {
   currentMillis = millis();
   if (currentMillis - previousMillis > interval) {
-
     pulse1Sec = pulseCount;
     pulseCount = 0;
 
-    // Because this loop may not complete in exactly 1 second intervals we calculate
-    // the number of milliseconds that have passed since the last execution and use
-    // that to scale the output. We also apply the calibrationFactor to scale the output
-    // based on the number of pulses per second per units of measure (litres/minute in
-    // this case) coming from the sensor.
     flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
     previousMillis = millis();
 
-    // Divide the flow rate in litres/minute by 60 to determine how many litres have
-    // passed through the sensor in this 1 second interval, then multiply by 1000 to
     // convert to millilitres.
     flowMilliLitres = (flowRate / 60) * 1000;
 
     // Add the millilitres passed in this second to the cumulative total
     totalMilliLitres += flowMilliLitres;
-
+    volume += totalMilliLitres / 1000;
     // Print the flow rate for this second in litres / minute
     Serial.print("Flow rate: ");
     Serial.print(int(flowRate));  // Print the integer part of the variable
@@ -263,23 +251,122 @@ void waterFlow() {
     Serial.print("Output Liquid Quantity: ");
     Serial.print(totalMilliLitres);
     Serial.print("mL / ");
-    Serial.print(totalMilliLitres / 1000);
+    Serial.print(volume);
     Serial.println("L");
 
     // Check if 10 seconds have passed since the last save
     if (currentMillis - lastSaveTime > 10000) {
       // Check if data is different from previous data before saving to LittleFS
-      if (flowRate != previousFlowRate || totalMilliLitres != previousTotalMilliLitres) {
+      if (totalMilliLitres != previousTotalMilliLitres) {
         // Simpan data ke LittleFS
-        writeFile(LittleFS, "/debit.txt", String(flowRate).c_str());
-        writeFile(LittleFS, "/volume.txt", String(totalMilliLitres / 1000).c_str());
+        writeFile(LittleFS, "/volume.txt", String(volume).c_str());
 
         // Update nilai-nilai sebelumnya
-        previousFlowRate = flowRate;
         previousTotalMilliLitres = totalMilliLitres;
+
         // Update waktu terakhir penyimpanan
         lastSaveTime = currentMillis;
       }
     }
   }
+}
+
+void reqTokenFromServer() {
+  HTTPClient http;
+
+  DynamicJsonDocument doc(1024);
+  doc["secret"] = "secret";
+  doc["id"] = 1;
+  doc["random"] = "TPjnu0HO";
+
+  // Mengonversi objek JSON ke string
+  String jsonString = "";
+  serializeJson(doc, jsonString);
+
+  // Kirim permintaan POST
+  http.begin(urlReqToken);
+  http.addHeader("Content-Type", "application/json");
+  Serial.print("jsonString : ");
+  Serial.println(jsonString);
+  int httpResponseCode = http.POST(jsonString);
+
+  // Jika permintaan berhasil
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+
+    String response = http.getString();
+    Serial.println(response);
+    //Deserialize dari json
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, response);
+
+    String token = doc["token"];
+    reqToken = token;
+    Serial.print("Token anda : ");
+    Serial.println(reqToken);
+  } else {
+    Serial.print("Error in HTTP request. HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    Serial.print("httpResponseCode : ");
+    Serial.println(httpResponseCode);
+  }
+  http.end();
+}
+
+void sendToServer() {
+  HTTPClient http;
+
+  DynamicJsonDocument doc(1024);
+
+  doc["token"] = reqToken;
+  doc["volume"] = volume;
+  doc["debit"] = int(flowRate);
+
+  // Mengonversi objek JSON ke string
+  String jsonString = "";
+  serializeJson(doc, jsonString);
+
+  // Kirim permintaan POST
+  http.begin(urlSendData);
+  http.addHeader("Content-Type", "application/json");
+  Serial.print("jsonString : ");
+  Serial.println(jsonString);
+  int httpResponseCode = http.POST(jsonString);
+
+  // Jika permintaan berhasil
+  if (httpResponseCode > 0) {
+    Serial.print("HTTP Response code: ");
+    Serial.println(httpResponseCode);
+
+    String response = http.getString();
+    Serial.println(response);
+    //Deserialize dari json
+    DynamicJsonDocument doc(1024);
+    deserializeJson(doc, response);
+
+    String msg = doc["msg"];
+    Serial.print("Keterangan : ");
+    Serial.println(msg);
+    if (msg.equals("success")) {
+      bacaRef();
+      volume = 0;
+    } else {
+      if (countSendToServer > 4) {
+        //HARDRESETTTTT
+      }
+      countSendToServer++;
+      sendToServer();
+    }
+  } else {
+    Serial.print("Error in HTTP request. HTTP Response code: ");
+    Serial.println(httpResponseCode);
+    Serial.print("httpResponseCode : ");
+    Serial.println(httpResponseCode);
+    if (httpResponseCode == -11) {
+      Serial.println("Resend");
+      sendToServer();
+    }
+  }
+  http.end();
 }
